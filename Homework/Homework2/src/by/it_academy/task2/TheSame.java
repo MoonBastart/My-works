ba6bac34755aa24d1a1079bc7b3ee5d9ce0dbc47
Jack <intergral@list.ru>
2017-11-19 23:03:12 +0300

    Done

--- Homework/Homework2/src/by/it_academy/task2/TheSame.java
+++ Homework/Homework2/src/by/it_academy/task2/TheSame.java
@@ -1,33 +1,22 @@
 package by.it_academy.task2;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
+
 public class TheSame {
     public static void main (String[] arg) {
 
 
-        int[] justArray2 = new int[] {7, 26, 4, 3, 4, 17, 13, 7, 7, 4}; //works only for positive values
-
-        System.out.print("Array: [");
-
-        for (int i = 0; i < justArray2.length - 1; i++) {
-            System.out.print(justArray2[i] + ", ");
-        }
-        System.out.println(justArray2[justArray2.length - 1] + "]");
-
-        int max = justArray2[0];
-        for (int i = 0; i < justArray2.length; i++) {
-            if (max < justArray2[i])
-                max = justArray2[i];
-        }
-
-        int[] counter = new int[max + 1];
+        float[] justArray2 = new float[] {7, 26, 4, 3, 12, 17, 13, 8, 7, 4}; //works only for positive values
 
         for (int i = 0; i < justArray2.length; i++) {
-            counter[justArray2[i]]++;
-        }
-
-        for (int i = 0; i < counter.length; i++) {
-            if (counter[i] > 1) {
-                System.out.println("[" + i + "] повторений [" + counter[i] + "]");
+            int counter = 0;
+            for (int j = i; j < justArray2.length; j++) {
+                if (justArray2[i] == justArray2[j]) {
+                    counter++;
+                }
+            }
+            if (counter > 1) {
+                System.out.println("[" + justArray2[i] + "] - is repeated " + counter + " times");
             }
         }
 
