a36169173f39ce5da6e470c0082f9db914468b81
Jack <intergral@list.ru>
2017-11-19 13:53:27 +0300

    Almost done

--- Homework/Homework2/src/by/it_academy/task4/Sequence.java
+++ Homework/Homework2/src/by/it_academy/task4/Sequence.java
@@ -4,10 +4,14 @@ public class Sequence {
     public static void main (String[] arg) {
 
 
-        /*int k = 4689;
-        if(k / 1000 < (k / 100) % 10 &&
-                (k / 100) % 10 < (k / 10) % 100 &&
-                (k / 10) % 100 < k /*/
+        int k = 1234;
+        if(k / 1000 + 1 == (k / 100) % 10 &&
+                (k / 100) % 10 + 1 == (k / 10) % 100 % 10 &&
+                (k / 10) % 100 % 10 + 1 == k % 1000 % 100 % 10) {
+            System.out.println("Yes, these numbers form an increasing sequence");
+        } else {
+            System.out.println("No, these numbers do not form an increasing sequence");
+        }
 
 
     }
