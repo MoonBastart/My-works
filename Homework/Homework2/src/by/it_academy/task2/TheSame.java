cc1046d6c801a3b6f137365a33f8559ded1d7db6
Jack <intergral@list.ru>
2017-11-26 21:25:44 +0300

    fixed

--- Homework/Homework2/src/by/it_academy/task2/TheSame.java
+++ Homework/Homework2/src/by/it_academy/task2/TheSame.java
@@ -6,18 +6,33 @@ public class TheSame {
     public static void main (String[] arg) {
 
 
-        float[] justArray2 = new float[] {4, 26, 4, 3, 7, 17, 13, 4, 7, 4};
+        float[] justArray2 = new float[] {5, 26, 5, 3, 2, 87, 13, 5, 7, 2};
+
+        boolean notFound = false;
+        boolean copy = false;
 
         for (int i = 0; i < justArray2.length; i++) {
             int counter = 0;
-            for (int j = i; j < justArray2.length; j++) {
+
+            for (int j = 0; j < justArray2.length; j++) {
                 if (justArray2[i] == justArray2[j]) {
                     counter++;
+                    if (i > j) {
+                        copy = true;
+                        break;
+                    }
                 }
             }
-            if (counter > 1) {
+            if (counter > 1 && copy == false) {
+                notFound = true;
                 System.out.println("[" + justArray2[i] + "] - is repeated " + counter + " times");
             }
+            counter = 0;
+            copy = false;
+        }
+
+        if (notFound == false) {
+            System.out.println("Coincidences do not found");
         }
 
 
--- Homework/Homework2/src/by/it_academy/task4/Sequence.java
+++ Homework/Homework2/src/by/it_academy/task4/Sequence.java
@@ -4,19 +4,19 @@ public class Sequence {
     public static void main (String[] arg) {
 
 
-        Integer number = 123456789;
+        Integer number = 1234567898;
         String newNumber = number.toString();
         char [] finalNumber = newNumber.toCharArray();
         boolean found = false;
         for (int i = 0; i < finalNumber.length - 1; i++) {
                 if (finalNumber[i] + 1 != finalNumber[i + 1]) {
                     found = true;
-                    System.out.println("No, these numbers do not form an increasing sequence");
+                    System.out.println("No, " + number + " numbers do not form an increasing sequence");
                     break;
                 }
             }
         if (found != true) {
-            System.out.println("Yes, these numbers form an increasing sequence");
+            System.out.println("Yes, " + number + " numbers form an increasing sequence");
         }
 
     }
